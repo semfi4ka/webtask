f1d0477a549984e0d1b6554e4b69832d439fc722
semfi4ka <semfi2006@gmail.com>
2025-12-08 18:58:33 +0300

    make fixes

--- src/main/java/com/filippovich/webtask/util/PasswordEncoder.java
+++ src/main/java/com/filippovich/webtask/util/PasswordEncoder.java
@@ -1,13 +1,18 @@
 package com.filippovich.webtask.util;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class PasswordEncoder {
 
+    private static final Logger logger = LogManager.getLogger(PasswordEncoder.class);
     private static final String FORMAT = "%02x";
     private static final String ALGORITHM = "SHA-256";
+
     public String hash(String rawPassword) {
         try {
             MessageDigest md = MessageDigest.getInstance(ALGORITHM);
@@ -18,10 +23,12 @@ public class PasswordEncoder {
                 stringBuilder.append(String.format(FORMAT, b));
             }
 
+            logger.debug("Password successfully hashed");
             return stringBuilder.toString();
 
         } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("SHA-256 algorithm not available", e); // заменю на логи!!!
+            logger.fatal("SHA-256 algorithm not available", e);
+            throw new RuntimeException("SHA-256 algorithm not available", e);
         }
     }
 }
